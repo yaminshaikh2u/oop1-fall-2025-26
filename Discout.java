229a21ca176ecdcb0ff400b74851e7d157a7f3b0
yamin <yaminshaikh@Yamins-MacBook-Air.local>
2025-10-30 00:57:39 +0600

    24-59763-3

---
+++ Discout.java
@@ -0,0 +1,11 @@
+public class Discout {
+public static void main(String[] args) {
+    
+            float product = 250f;
+            float percentage = (float) (250 * 0.15);
+            float discount = product - percentage;
+            System.out.println("After 15% discount the price is : " + discount);
+        }
+    }
+
+    
