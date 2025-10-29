c122a070f117c7d14baacc10f6c1b2ee37f17a75
yamin <yaminshaikh@Yamins-MacBook-Air.local>
2025-10-30 01:04:04 +0600

    24-59763-3

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+        public static void main(String[] args) {
+           float student1 = 70;
+           float student2 =80;
+           float student3 =90;
+           float average = (70+80+90)/3;
+            System.out.println("The average number of 3 students : " + average);
+        }
+    }
+
