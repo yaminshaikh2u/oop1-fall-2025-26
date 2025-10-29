fc380f0d427af311484d3bfac3831cd4eee7f575
yamin <yaminshaikh@Yamins-MacBook-Air.local>
2025-10-30 01:15:48 +0600

    24-59763-3

---
+++ Yamin.java
@@ -0,0 +1,9 @@
+public class Yamin {
+        public static void main(String[] args) {
+            int temperatureC = 35;
+            float temperatureF = (temperatureC/5*9)+32 ;
+    System.out.println(" 35 Degree Celcius to Fahrenheit is :" + temperatureF);
+    
+        }
+    }
+
